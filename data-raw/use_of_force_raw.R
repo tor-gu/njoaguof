8722746ffdf91497396caef368873475eb5a2621
tor-gu <tor.gunston@protonmail.com>
2022-03-18 10:59:06 -0400

    Removing the stata format labels introduced by haven.

--- data-raw/use_of_force_raw.R
+++ data-raw/use_of_force_raw.R
@@ -3,6 +3,7 @@ use_of_force_raw <-
               "NJOAGUOF_Data_100120_to_022822.dta",
               package = "njoaguof") %>%
   haven::read_dta() %>%
+  haven::zap_formats() %>%
   dplyr::mutate(dplyr::across(
     where(rlang::is_character), stringr::str_trim))
 
