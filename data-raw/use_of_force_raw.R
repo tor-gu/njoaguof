5376402f57a97a76a6eccbb71dd1ea59407e05eb
tor-gu <tor.gunston@protonmail.com>
2026-09-06 22:15:55 -0400

    move dta file to data-raw and don't ship it

--- data-raw/use_of_force_raw.R
+++ data-raw/use_of_force_raw.R
@@ -1,11 +1,13 @@
+# The raw .dta is build-time input only. It lives in data-raw/ (which is
+# .Rbuildignore'd) so that it does not ship with the installed package --
+# installed users get the much smaller data/use_of_force_raw.rda instead.
+library(dplyr)
+
 use_of_force_raw <-
-  system.file("extdata",
-              "NJOAGUOF_Data_100120_to_103124.dta",
-              package = "njoaguof") %>%
+  "data-raw/NJOAGUOF_Data_100120_to_103124.dta" %>%
   haven::read_dta() %>%
   haven::zap_formats() %>%
   dplyr::mutate(dplyr::across(
     where(rlang::is_character), stringr::str_trim))
 
 usethis::use_data(use_of_force_raw, overwrite = TRUE)
-
